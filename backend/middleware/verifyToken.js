b7e006665da76e5f7941be1c5bee4caa2cf4c95e
Juan11211 <avilesjuan628@gmail.com>
2023-05-08 11:28:03 -0400

    Working on frontend now

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -13,8 +13,11 @@ const verifyToken = async (req, res, next) => {
     }
 
     const verified = jwt.verify(token, process.env.JWT_SECRET);
+    if (!verified) {
+      return res.status(403).send("Invalid token");
+    }
+    
     req.auth = verified;
-   // console.log('User ID:', req.auth);
     next();
   } catch (err) {
     res.status(500).json({ error: err.message });
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUser, follow, getUser, unFollow, update } from '../controllers/userController.js';
+import { deleteUser, getUser, update } from '../controllers/userController.js';
 import verifyToken from '../middleware/verifyToken.js';
 
 const userRouter = express.Router();
@@ -10,8 +10,5 @@ userRouter.put('/:id', verifyToken, update)
 
 userRouter.delete('/:id', verifyToken, deleteUser)
 
-userRouter.put('/follow/:id', verifyToken, follow)
-
-userRouter.put('/unfollow/:id', verifyToken, unFollow)
 
 export default userRouter;
